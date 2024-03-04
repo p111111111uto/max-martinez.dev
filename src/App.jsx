e5470b9d14854f1b50a3b31326ab4b769020e590
max <mmfern2003@gmail.com>
2024-03-04 09:42:10 -0500

    updating website info

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import '@fontsource/roboto'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      
     </>
   )
 }
