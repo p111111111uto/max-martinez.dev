0d6af95a4a7288ff7f60362de0e0311f9e540a81
max <mmfern2003@gmail.com>
2024-03-05 10:07:05 -0500

    Importing Header and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './components/Header';
+import Home from './components/Home/Home';
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Home />
     </>
   )
 }
