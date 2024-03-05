20093ae0a9bc000171b8b72c3228b7ef59780b7f
max <mmfern2003@gmail.com>
2024-03-05 16:39:14 -0500

    Adding Earth image to second parallax layer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,31 @@
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Type from './Type';
+import moon from '../assets/moon.jpg';
+import earth from '../assets/earth.jpg';
 
 export default function Home() {
     return (
         <>
             <Parallax pages={2}>
-                <ParallaxLayer speed={1}>
+                <ParallaxLayer
+                speed={1}
+                factor={2}
+                style={{
+                    backgroundImage: `url(${moon})`,
+                    backgroundSize: 'cover'
+                }}>
                     <h2>Hello there!</h2>
                     <h1>My name is MAX MARTINEZ</h1>
                     <Type />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={1} speed={0.5}>
+                <ParallaxLayer
+                offset={1}
+                speed={0.5}
+                style={{
+                    backgroundImage: `url(${earth})`,
+                    backgroundSize: 'cover'
+                }}>
                     <h1>Who am I?</h1>
                     <h2>I started learning how to code in 2022 and since then, I&apos;ve loved it!</h2>
                     <h2>I enjoy building visually appealing fullstack applications and websites.</h2>
