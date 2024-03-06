158eb3ff5bb8e4728da39ace79868c30df110284
max <mmfern2003@gmail.com>
2024-03-06 11:11:11 -0500

    Adding stars as a middle parallax layer for space travelling effect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,27 +2,42 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Type from './Type';
 import moon from '../assets/moon.jpg';
 import earth from '../assets/earth.jpg';
+import stars from '../assets/stars.jpg';
 
 export default function Home() {
     return (
         <>
-            <Parallax pages={2}>
+            <Parallax pages={3}>
                 <ParallaxLayer
+                offset={0}
                 speed={1}
                 factor={2}
                 style={{
                     backgroundImage: `url(${moon})`,
-                    backgroundSize: 'cover'
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
                 }}>
                     <div className="parallaxMain">
                         <h1>Hello Out There!</h1>
-                        <h1>Come closer to my world...</h1>
+                        <h1>Welcome to my world...</h1>
                     </div>
                 </ParallaxLayer>
 
                 <ParallaxLayer
-                offset={1}
-                speed={0.5}
+                    offset={1}
+                    style={{
+                        backgroundImage: `url(${stars})`,
+                        backgroundSize: 'cover',
+                    }}
+                    speed={1.5}
+                    factor={2}
+                >
+
+                </ParallaxLayer>
+
+                <ParallaxLayer
+                offset={2}
+                speed={1}
                 factor={2}
                 style={{
                     backgroundImage: `url(${earth})`,
@@ -33,13 +48,13 @@ export default function Home() {
                         <div style={{fontSize: '30px', fontWeight: 'bold', color: '#7393B3'}}>
                             <Type />
                         </div>
-                        <p className="bodyText">
-                            My name is Max Martinez and I am an aspiring software engineer.
+                        <h3 className="bodyText">
+                            My name is <span style={{color: '#7393B3'}}>Max Martinez</span> and I am an aspiring software engineer.
                             <br />
                             I started learning how to code in 2022 and since then, I&apos;ve loved it!
                             <br />
-                            I am currently taking the University of Miamis fullstack coding bootcamp where I learned to create this website!
-                        </p>
+                            I am currently taking a fullstack coding bootcamp at the University of Miami where I learned to create this website!
+                        </h3>
                     </div>
                 </ParallaxLayer>
             </Parallax>
