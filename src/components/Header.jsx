60d51f743ba9575ec2bd03ef987df3850a6f349f
max <mmfern2003@gmail.com>
2024-03-06 15:08:09 -0500

    styling to navbar/header as well as links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navigation from "./Navigation"
 
 export default function Header() {
     return (
-        <header>
+        <header className="headerContainer">
             <h1 style={{color: '#7393B3', marginLeft:'20px'}}>Mm.</h1>
             <Navigation />
         </header>
