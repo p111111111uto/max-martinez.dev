918b30130d17db5b21b6a739f8a8736b8ce3f90f
max <mmfern2003@gmail.com>
2024-03-06 12:54:33 -0500

    Routing all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
 import Header from './components/Header';
 import Home from './components/Home/Home';
+import AboutMe from './components/AboutMe';
+import ContactMe from './components/ContactMe';
+import Projects from './components/Projects';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about-me' element={<AboutMe />} />
+          <Route path='/contact-me' element={<ContactMe />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Router>
       <Header />
     </>
   )
