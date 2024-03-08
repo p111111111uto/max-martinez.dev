a07335ee3f302c51ba98cf3d06df74edfa8d0ed5
max <mmfern2003@gmail.com>
2024-03-07 21:21:44 -0500

    Particle addition

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -1,24 +1,28 @@
 import SkillSet from "./SkillSet"
 import Tools from "./Tools"
+import Particle from "../Particle"
 
 export default function AboutMe() {
     return (
         <div className="aboutMe">
-            <h1>Get to know <span>me</span></h1>
+            <Particle />
+            <h1 id="firstH">Get to know <span style={{color:'#7393B3'}}> me</span></h1>
             <div className="aboutBody">
-                <p>
-                    Hi everyone! My name is Max and as previously mentioned, I am an aspiring software engineer.
-                    <br />
-                    Throughout my life I have enjoyed computers, I always found it fascinating how the way that computers are able to do such complex things seems like magic.
-                    <br />
-                    However, as most of us in the field know and how my instructor always says, it is far from magic.
-                    <br />
-                    Actually, it is even more fascinating understanding how they work and why they work. I find great joy in creating something from scratch and watch it come to life in real time. Which is exactly why I do what I do.
-                    <br />
-                    I am currently attending a fullstack coding bootcamp at the University of Miami and currently work there as a desktop support technician.
-                </p>
+                <div className="details">
+                    <p>
+                        Hi everyone! My name is Max and as previously mentioned, I am an aspiring software engineer.
+                        <br />
+                        Throughout my life I have enjoyed computers, I always found it fascinating how the way that computers are able to do such complex things seems like magic.
+                        <br />
+                        However, as most of us in the field know and how my instructor always says, it is far from magic.
+                        <br />
+                        Actually, it is even more fascinating understanding how they work and why they work. I find great joy in creating something from scratch and watch it come to life in real time. Which is exactly why I do what I do.
+                        <br />
+                        I am currently attending a fullstack coding bootcamp at the University of Miami and currently work there as a desktop support technician.
+                    </p>
+                </div>
                 <div className="extra">
-                    <p>Aside from programming I love:</p>
+                    <p>Aside from programming I love...</p>
                     <ul>
                         <li>Animals</li>
                         <li>Video Games</li>
@@ -29,7 +33,7 @@ export default function AboutMe() {
                     </ul>
                 </div>
                 <div className="skillSet">
-                    <h1><span style={{color:'#7393B3'}}>Languages</span> I Use</h1>
+                    <h1><span style={{color:'#7393B3'}}> Languages </span> I Use</h1>
                     <SkillSet />
                 </div>
                 <div className="toolSet">
