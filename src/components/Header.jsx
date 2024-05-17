4abd8ae9c42bf76b8c00d2ce729923988e4b6ef3
Max Martinez <mmfern2003@gmail.com>
2024-05-16 23:51:37 -0400

    add source code button to header + styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import DropDown from "./DropDown";
+import { BiGitBranch,
+    BiSolidStar
+ } from "react-icons/bi";
 
 export default function Header() {
     return (
         <header className="headerContainer">
             <DropDown />
+            <div className="sourceCode">
+                <a href="https://github.com/maxmruiz/max-martinez.dev.git" target='_blank'><BiGitBranch/><BiSolidStar/></a>
+            </div>
         </header>
     );
 }
