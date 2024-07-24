e1a4f028bb486344cc2ceecc48cb2852b889e3f2
Maria Martinez <mariamartinez@Marias-MacBook-Pro.local>
2024-07-24 14:02:22 -0400

    resumes changes

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,4 +1,5 @@
 import resume from './assets/resume.jpg'
+import resumepdf from './assets/resumepdf.pdf'
 import './assets/style.css'
 
 export default function Resume() {
@@ -6,7 +7,7 @@ export default function Resume() {
         <div className="resumeContainer">
             <h1>My Resume</h1>
             <img src={resume} alt="resume" />
-            <a href={resume} download className='downloadBtn'>Download</a>
+            <a href={resumepdf} download className='downloadBtn'>Download</a>
         </div>
     )
 }
